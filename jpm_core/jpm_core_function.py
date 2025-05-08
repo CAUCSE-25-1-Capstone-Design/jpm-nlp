95e89caa4b873e60f2d53ff7b0c96794a6b7f3cc
김두회 <duhoe@MacBook-Air-2.local>
2025-05-09 01:27:03 +0900

    feat: install 연동

--- jpm_core/jpm_core_function.py
+++ jpm_core/jpm_core_function.py
@@ -1,7 +1,11 @@
 
+import subprocess
+import json
+
 #tool_call을 받아서 파싱한 후 jpm 호출
 def jpm_caller(tool_call):
     name=tool_call.name
+    args = json.loads(tool_call.arguments)
     print("함수명:" + name)
 
     #  함수명에 따라 jpm-core 호출해야 함.
@@ -9,8 +13,22 @@ def jpm_caller(tool_call):
         case "installPkg":
 
             ## to be implemented
+            # organization:name으로 붙여.
+            org_and_name=args["organization"]+":"+args["name"]
+            result = subprocess.run(
+                ['java', '-jar', 'C:\\Users\\PC\\Desktop\\Python\\jpm-nlp\\jpm_core\\jpm.jar', 'install', org_and_name],
+                capture_output=True,
+                text=True
+            )
 
-            return "패키지를 설치하였습니다."
+            # print(result.stdout)
+
+            # print("STDERR:")
+            # print(result.stderr)
+            # 추후에 예외처리 필요.
+
+
+            return result.stdout
         case "deletePkg":
             
             return "패키지를 삭제했습니다."
